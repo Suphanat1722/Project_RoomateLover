184fcfbc3c87bb5626d3d16d972dd3322f081130
Suphanat1722 <suphanat1722@gmail.com>
2024-12-24 14:40:31 +0700

    feat: ทำให้ปุ่มต่างๆใช้งานได้
    เพิ่มลดค่าความสงสัย
    เพิ่มการเปลี่ยนฉากไปมา

---
+++ Assets/Scrips/SceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // เปลี่ยน Scene โดยใช้ Index (จาก Build Settings)
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }   
+}
