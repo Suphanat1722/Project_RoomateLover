f2eb4e600c5ba7947f75bc2d08af85930fe7ec1d
Suphanat1722 <suphanat1722@gmail.com>
2025-02-04 16:18:54 +0700

    perf: ปรับปรุงโค้ดในการทำงานแสดงปุ่ม
    - แยกการทำงานมือซ้ายกับขวา
    - การแสดงชื่อว่ามือไหนเลือกอะไรอยู่

--- Assets/Scrips/Nigh Time Main Game/ButtonNightitmeController.cs
+++ Assets/Scrips/Nigh Time Main Game/ButtonNightitmeController.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,44 +9,46 @@ public class ButtonNighttimeController : MonoBehaviour
     public GameObject shirt;
     public GameObject shorts;
     public GameObject underwear;
-    public GameObject hip1,hip2;
-    public GameObject leg1,leg2;
+    public GameObject hipClosed, hipOpened;
+    public GameObject legClosed, legOpened;
 
     [Header("Part Collider")]
     public GameObject bodyUpperWearShirtCollider;
     public GameObject bodyUpperNotWearShirtCollider;
     public GameObject bodyUnderWearShortsCollider;
     public GameObject bodyUnderNotWearShortsCollider;
-    public GameObject breastCollider;
-    public GameObject leg1Collider, leg2Collider;
-    public GameObject pussy1Collider, pussy2Collider;
-
+    public GameObject breastRightCollider, breastLeftCollider;
+    public GameObject legClosedCollider, legOpenedCollider;
+    public GameObject pussyClosedCollider, pussyOpenedCollider;
 
     [Header("UI Button")]
     public GameObject bodyUpperButton;
     public GameObject bodyUnderButton;
-    public GameObject breastButton;
+    public GameObject breastLbutton, breastRbutton;
     public GameObject legsButton;
-    public GameObject pussy1Button, pussy2Button;
+    public GameObject closedLegsPussyLeftButton;
+    public GameObject closedLegsPussyRightButton;
+    public GameObject openLegsPussyLeftButton;
+    public GameObject openLegsPussyRightButton;
 
     [Header("Text")]
-    public TextMeshProUGUI textSelectLayerName;
+    public TextMeshProUGUI textLeftHand;
+    public TextMeshProUGUI textRightHand;
 
     private int shirtLevel;
     private int shortsLevel;
     private string currentLayerName;
-    private string currentSelectLayerName;
-    private bool IsSpreadlegs;
-
-    private int beforeStatus; // 1 = standard
-    private int currentStatus;
+    private string selectedLayerLeft = "Left Hand";
+    private string selectedLayerRight = "Right Hand";
+    private bool isSpreadLegs;
 
     private void Update()
     {
         HandleMouseInput();
         CheckClothingLevels();
 
-        textSelectLayerName.text = currentSelectLayerName;
+        textLeftHand.text = selectedLayerLeft;
+        textRightHand.text = selectedLayerRight;
     }
 
     #region Interaction Methods
@@ -54,13 +57,16 @@ public class ButtonNighttimeController : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
+            selectedLayerLeft = "Left Hand";
+            selectedLayerRight = "Right Hand";
+
             DetectHoveredLayer();
-            ShowClothingButtons();
+            ShowUiButtons();
         }
         if (Input.GetMouseButtonDown(1))
         {
             currentLayerName = "cancel";
-            ShowClothingButtons();
+            ShowUiButtons();
         }
     }
 
@@ -71,100 +77,93 @@ public class ButtonNighttimeController : MonoBehaviour
 
         if (hit.collider != null)
         {
-            currentLayerName = LayerMask.LayerToName(hit.collider.gameObject.layer);
+            if (IsLayerValid(hit.collider.gameObject.layer))
+            {
+                currentLayerName = LayerMask.LayerToName(hit.collider.gameObject.layer);
+                Debug.Log("Current Layer Name: " + currentLayerName);
+            }
         }
     }
 
-    private void ShowClothingButtons()
+    private bool IsLayerValid(int layer)
     {
-        // Reset all buttons to inactive
-        bodyUpperButton.SetActive(false);
-        bodyUnderButton.SetActive(false);
-        breastButton.SetActive(false);
-        legsButton.SetActive(false);
-        pussy1Button.SetActive(false);
-        pussy2Button.SetActive(false);
+        string[] validLayers = { "Body Upper", "Body Under", "Breast L", "Breast R", "Legs", "Pussy" };
+        string layerName = LayerMask.LayerToName(layer);
+        return System.Array.IndexOf(validLayers, layerName) != -1;
+    }
 
-        // Activate the appropriate button based on the current layer
+    private void ShowUiButtons()
+    {
+        ResetAllButtons();
+
         switch (currentLayerName)
         {
+            case "Breast L":
+                breastLbutton.SetActive(true);
+                selectedLayerLeft = "Left Breast";
+                break;
+            case "Breast R":
+                breastRbutton.SetActive(true);
+                selectedLayerRight = "Right Breast";
+                break;
             case "Body Upper":
-
                 bodyUpperButton.SetActive(true);
-
-                currentSelectLayerName = "เสื้อ";
+                selectedLayerRight = "เสื้อ";
                 break;
-
             case "Body Under":
-
                 bodyUnderButton.SetActive(true);
-
-                if (shortsLevel < 3)
-                {
-                    currentSelectLayerName = "กางเกง";
-                }
-                else
-                {
-                    currentSelectLayerName = "กางเกงใน";
-                }
+                selectedLayerRight = shortsLevel < 3 ? "กางเกง" : "กางเกงใน";
                 break;
-
-            case "Breast":
-                
-                breastButton.SetActive(true);
-
-                currentSelectLayerName = "หน้าอก";
-                break;
-
             case "Legs":
-                
                 legsButton.SetActive(true);
-
-                currentSelectLayerName = "ขา";
+                selectedLayerRight = "Legs";
                 break;
-
             case "Pussy":
-
-                if (!IsSpreadlegs)
+                if (isSpreadLegs)
                 {
-                    pussy1Button.SetActive(true);
+                    openLegsPussyLeftButton.SetActive(true);
+                    openLegsPussyRightButton.SetActive(true);
                 }
                 else
                 {
-                    pussy2Button.SetActive(true);
+                    closedLegsPussyLeftButton.SetActive(true);
+                    closedLegsPussyRightButton.SetActive(true);
                 }
-
-                currentSelectLayerName = "จิ๋ม";
+                selectedLayerLeft = selectedLayerRight = "Pussy";
                 break;
             case "cancel":
-
-                currentSelectLayerName = "เลือก";
-
-                bodyUpperButton.SetActive(false);
-                bodyUnderButton.SetActive(false);
-                breastButton.SetActive(false);
-                legsButton.SetActive(false);
-                pussy1Button.SetActive(false);
-                pussy2Button.SetActive(false);
-                break;
-
-            default:
-                // No layer selected; keep all buttons inactive
+                selectedLayerLeft = "Left Hand";
+                selectedLayerRight = "Right Hand";
                 break;
         }
     }
+
+    private void ResetAllButtons()
+    {
+        bodyUpperButton.SetActive(false);
+        bodyUnderButton.SetActive(false);
+        breastLbutton.SetActive(false);
+        breastRbutton.SetActive(false);
+        legsButton.SetActive(false);
+        closedLegsPussyLeftButton.SetActive(false);
+        closedLegsPussyRightButton.SetActive(false);
+        openLegsPussyLeftButton.SetActive(false);
+        openLegsPussyRightButton.SetActive(false);
+    }
+
     #endregion
+
     #region Button Methods
 
     public void TakeOffClothes()
     {
         if (currentLayerName == "Body Upper")
         {
-            shirtLevel++;
+            shirtLevel = Mathf.Clamp(shirtLevel + 1, 0, 3);
         }
         else if (currentLayerName == "Body Under")
         {
-            shortsLevel++;
+            shortsLevel = Mathf.Clamp(shortsLevel + 1, 0, 6);
         }
     }
 
@@ -172,123 +171,57 @@ public class ButtonNighttimeController : MonoBehaviour
     {
         if (currentLayerName == "Body Upper")
         {
-            shirtLevel--;
+            shirtLevel = Mathf.Clamp(shirtLevel - 1, 0, 3);
         }
         else if (currentLayerName == "Body Under")
         {
-            shortsLevel--;
+            shortsLevel = Mathf.Clamp(shortsLevel - 1, 0, 6);
         }
     }
 
-    public void SpreadLegs()
+    private void SetLegsState(bool spread)
     {
-        IsSpreadlegs = true;
+        isSpreadLegs = spread;
 
-        hip1.SetActive(false);
-        hip2.SetActive(true);
+        hipClosed.SetActive(!spread);
+        hipOpened.SetActive(spread);
 
-        leg1.SetActive(false);
-        leg2.SetActive(true);
+        legClosed.SetActive(!spread);
+        legOpened.SetActive(spread);
 
-        leg1Collider.SetActive(false);
-        leg2Collider.SetActive(true);
+        legClosedCollider.SetActive(!spread);
+        legOpenedCollider.SetActive(spread);
+
+        pussyClosedCollider.SetActive(!spread);
+        pussyOpenedCollider.SetActive(spread);
     }
-    
-    public void FoldLegsBack() 
-    {
-        IsSpreadlegs = false;
 
-        hip1.SetActive(true);
-        hip2.SetActive(false);
+    public void SpreadLegs() => SetLegsState(true);
+    public void FoldLegsBack() => SetLegsState(false);
 
-        leg1.SetActive(true);
-        leg2.SetActive(false);
-
-        leg1Collider.SetActive(true);
-        leg2Collider.SetActive(false);
-    }
     #endregion
 
     private void CheckClothingLevels()
     {
-        shirtLevel = Mathf.Clamp(shirtLevel, 0, 3);
-        shortsLevel = Mathf.Clamp(shortsLevel, 0, 6);
+        shirt.SetActive(shirtLevel != 3);
+        bodyUpperWearShirtCollider.SetActive(shirtLevel != 3);
+        bodyUpperNotWearShirtCollider.SetActive(shirtLevel == 3);
+        breastLeftCollider.SetActive(shirtLevel == 3);
+        breastRightCollider.SetActive(shirtLevel == 3);
 
-        //เสื้อ
-        if (shirtLevel == 3)
-        {
-            //ถอดเสื้อ
+        bool isUnderwearRemoved = shortsLevel >= 6;
+        bool isShortsRemoved = shortsLevel >= 3;
 
-            //ส่วนเสื้อผ้า
-            shirt.SetActive(false);
-            //ส่วนร่างกาย
-            bodyUpperWearShirtCollider.SetActive(false);
-            bodyUpperNotWearShirtCollider.SetActive(true);          
-            breastCollider.SetActive(true);
-        }
-        else
-        {
-            //ไม่ถอดเสื้อ
+        bodyUnderWearShortsCollider.SetActive(!isUnderwearRemoved);
+        bodyUnderNotWearShortsCollider.SetActive(isUnderwearRemoved);
 
-            //ส่วนเสื้อผ้า
-            shirt.SetActive(true);
+        legClosedCollider.SetActive(!isShortsRemoved);
+        legOpenedCollider.SetActive(isUnderwearRemoved);
 
-            //ส่วนร่างกาย
-            bodyUpperWearShirtCollider.SetActive(true);
-            bodyUpperNotWearShirtCollider.SetActive(false);        
-            breastCollider.SetActive(false);
-        }
+        pussyClosedCollider.SetActive(!isSpreadLegs && isUnderwearRemoved);
+        pussyOpenedCollider.SetActive(isSpreadLegs && isUnderwearRemoved);
 
-        //กางเกง และ กางเกงใน
-        if (shortsLevel >= 6)
-        {
-            //ถอดกางเกงใน
-
-            //ส่วนร่างกาย
-            bodyUnderWearShortsCollider.SetActive(false);
-            bodyUnderNotWearShortsCollider.SetActive(true);         
-            leg1Collider.SetActive(true);
-            if (!IsSpreadlegs)
-            {
-                pussy1Collider.SetActive(true);
-            }
-            else
-            {
-                pussy2Collider.SetActive(true);
-            }        
-
-            //ส่วนเสื้อผ้า
-            shorts.SetActive(false);
-            underwear.SetActive(false);
-        }
-        else if (shortsLevel >= 3)
-        {
-            //ถอดกางเกง
-
-            //ส่วนร่างกาย
-            bodyUnderWearShortsCollider.SetActive(true);
-            bodyUnderNotWearShortsCollider.SetActive(false);
-            leg1Collider.SetActive(false);
-            pussy1Collider.SetActive(false);
-            pussy2Collider.SetActive(false);
-
-            //ส่วนเสื้อผ้า
-            shorts.SetActive(false);
-            underwear.SetActive(true);
-        }
-        else
-        {
-            //ไม่ถอดส่วนล่าง
-            bodyUnderWearShortsCollider.SetActive(true);
-            bodyUnderNotWearShortsCollider.SetActive(false);
-            leg1Collider.SetActive(false);
-            pussy1Collider.SetActive(false);
-            pussy2Collider.SetActive(false);
-
-
-            //ส่วนเสื้อผ้า
-            shorts.SetActive(true);
-            underwear.SetActive(true);
-        }
+        shorts.SetActive(!isShortsRemoved);
+        underwear.SetActive(isShortsRemoved && !isUnderwearRemoved);
     }
 }
