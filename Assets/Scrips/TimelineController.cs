e3f6a30d014ad35b0843e106575412a1c9b7611a
Suphanat1722 <suphanat1722@gmail.com>
2025-02-26 14:39:13 +0700

    feat: ย้ายระบบคัตซีน
    ย้านมาไว้ในซีนห้องหลัก

--- Assets/Scrips/TimelineController.cs
+++ Assets/Scrips/TimelineController.cs
@@ -3,18 +3,41 @@ using UnityEngine.Playables;
 
 public class TimelineController : MonoBehaviour
 {
-     public PlayableDirector pl_GoSchool;
+    public GameObject obj_Blur;
+    public GameObject obj_Frame;
+    public PlayableDirector pl_GoSchool;
+
+    void Start()
+    {
+        pl_GoSchool.stopped += OnPlayableDirectorStopped;
+    }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) // ถ้ากด Spacebar ให้เริ่มเล่น Timeline
+        if (Input.GetKeyDown(KeyCode.Space)) // ถ้ากด Spacebar ให้เริ่มเล่น Timeline
         {
             PlayTimelineGoToSchool();
-        }     
+        }
     }
 
     public void PlayTimelineGoToSchool()
     {
+        obj_Frame.SetActive(true); // แสดง Frame ที่ซ่อนไว้
+        obj_Blur.SetActive(true); // แสดง Blur ที่ซ่อนไว้
         pl_GoSchool.Play(); // ใช้ฟังก์ชันนี้เพื่อสั่งเล่น Timeline
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector director)
+    {
+        obj_Frame.SetActive(false);
+        obj_Blur.SetActive(false);
+
+        Debug.Log("Timeline has stopped.");
+    }
+
+    void OnDestroy()
+    {
+        // ลบ Event Listener เมื่อ Object ถูกทำลาย
+        pl_GoSchool.stopped -= OnPlayableDirectorStopped;
     }
 }
